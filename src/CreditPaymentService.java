fd9b2d3c107c4be71bdbd02f29c031bb5872b0cf
Anastasia Treshchina <anastasiya.treschina@gmail.com>
2022-02-06 19:09:11 +0400

    formula change

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
-    public float calculate(float amountOfCredit, float creditTerm) {
-        float creditRate = (float) 9.99;
+    public float calculate(float amountOfCredit, float creditTerm, float creditRate) {
         float loanRatePerMonth = (creditRate / 100) / 12;
-        float monthlyPayment = amountOfCredit * (loanRatePerMonth + (loanRatePerMonth / (((float) Math.pow( 1 + loanRatePerMonth, creditTerm) - 1))));
+        float monthlyPayment = amountOfCredit * (loanRatePerMonth + (loanRatePerMonth /
+                (((float) Math.pow( 1 + loanRatePerMonth, creditTerm) - 1))));
 
         return monthlyPayment;
     }
--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-        float monthlyPayment_12 = service.calculate(1_000_000, 12);
+        float monthlyPayment_12 = service.calculate(1_000_000, 12, 9.99f);
         String result_12 = String.format("%.0f", monthlyPayment_12);
         System.out.println("Ежемесячный платеж составит: " + result_12 + " рублей");
 
-        float monthlyPayment_24 = service.calculate(1_000_000, 24);
+        float monthlyPayment_24 = service.calculate(1_000_000, 24, 9.99f);
         String result_24 = String.format("%.0f", monthlyPayment_24);
         System.out.println("Ежемесячный платеж составит: " + result_24 + " рублей");
 
-        float monthlyPayment_36 = service.calculate(1_000_000, 36);
+        float monthlyPayment_36 = service.calculate(1_000_000, 36, 9.99f);
         String result_36 = String.format("%.0f", monthlyPayment_36);
         System.out.println("Ежемесячный платеж составит: " + result_36 + " рублей");
     }
