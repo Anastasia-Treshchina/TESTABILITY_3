f5b3ece47f80e2765cf3178fd640f7f25f3a63ce
Anastasia Treshchina <anastasiya.treschina@gmail.com>
2022-02-06 18:52:20 +0400

    Add 3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        float monthlyPayment = service.calculate(1_000_000, 24);
-        String result = String.format("%.0f", monthlyPayment);
-        System.out.println("Ежемесячный платеж составит: " + result + " рублей");
+
+        float monthlyPayment_12 = service.calculate(1_000_000, 12);
+        String result_12 = String.format("%.0f", monthlyPayment_12);
+        System.out.println("Ежемесячный платеж составит: " + result_12 + " рублей");
+
+        float monthlyPayment_24 = service.calculate(1_000_000, 24);
+        String result_24 = String.format("%.0f", monthlyPayment_24);
+        System.out.println("Ежемесячный платеж составит: " + result_24 + " рублей");
+
+        float monthlyPayment_36 = service.calculate(1_000_000, 36);
+        String result_36 = String.format("%.0f", monthlyPayment_36);
+        System.out.println("Ежемесячный платеж составит: " + result_36 + " рублей");
     }
 }
